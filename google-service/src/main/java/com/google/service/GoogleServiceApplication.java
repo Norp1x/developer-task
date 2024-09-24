ee9115ef361d9c8aaa6deb6746f7ce0ae4a0a1f4
Norpix <dudek.g@outlook.com>
2024-09-24 19:03:01 +0200

    Fixed naming, added search-service main class

--- google-service/src/main/java/com/google/service/GoogleServiceApplication.java
+++ google-service/src/main/java/com/google/service/GoogleServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SearchProjectApplication {
+public class GoogleServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SearchProjectApplication.class, args);
+		SpringApplication.run(GoogleServiceApplication.class, args);
 	}
 
 }
